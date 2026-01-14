e502cdac4694c971b7ad272271154f68c7b3746e
erymuzuan <erymuzuan@hotmail.com>
2026-01-14 14:33:51 +0800

    Build script

---
+++ build.web.ps1
@@ -0,0 +1,10 @@
+#web.core
+$cpwd = $PWD
+Get-ChildItem .\output\web.motorent | Remove-Item -Force -Recurse
+& dotnet publish ".\src\MotoRent.Server\MotoRent.Server.csproj" -c "Debug" -o "$cpwd\output\web.motorent" -p:UseAppHost=false
+
+# Remove-Item .\output\web.motorent\wwwroot\assets\ -Recurse -Force
+
+
+explorer .\output\
+
