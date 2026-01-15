8c12c25d7a38519f191e6cef4394c8649e134f28
erymuzuan <erymuzuan@hotmail.com>
2026-01-15 17:52:07 +0800

    feat: Add TestAlert and TestAlertService classes for maintenance status handling

---
+++ src/MotoRent.Domain/Entities/TestAlert.cs
@@ -0,0 +1,7 @@
+namespace MotoRent.Domain.Entities;
+
+public class TestAlert
+{
+    public MaintenanceStatus Status { get; set; }
+    public MaintenanceAlert? Alert { get; set; }
+}
---
+++ src/MotoRent.Services/TestAlertService.cs
@@ -0,0 +1,9 @@
+using MotoRent.Domain.Entities;
+
+namespace MotoRent.Services;
+
+public class TestAlertService
+{
+    public MaintenanceStatus Status { get; set; }
+    public MaintenanceAlert? Alert { get; set; }
+}
