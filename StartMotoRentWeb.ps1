e16480e6caf4be53b01418505301076f7404796e
erymuzuan <erymuzuan@hotmail.com>
2026-01-15 17:51:04 +0800

    feat: Add launch settings and environment variable logging to MotoRent Web Application startup script

--- StartMotoRentWeb.ps1
+++ StartMotoRentWeb.ps1
@@ -1,1 +1,9 @@
+$launchSettingsPath = Join-Path $PSScriptRoot "src\MotoRent.Server\Properties\launchSettings.json"
+$launchSettings = Get-Content $launchSettingsPath -Raw | ConvertFrom-Json
+$applicationUrl = $launchSettings.profiles.https.applicationUrl
+Write-Host "Starting MotoRent Web Application... $applicationUrl"
+
+$envUrl = $env:MOTO_BaseUrl
+Write-Host "env:MOTO_BaseUrl ... $envUrl"
+
 dotnet watch --project .\src\MotoRent.Server\MotoRent.Server.csproj
