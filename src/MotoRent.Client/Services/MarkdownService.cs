b637a79bab5a8bf83abc20d6fb7b2677bef90de8
erymuzuan <erymuzuan@hotmail.com>
2026-01-26 15:36:17 +0800

    fix: Resolve HttpClient InvalidOperationException by using absolute URIs

--- src/MotoRent.Client/Services/MarkdownService.cs
+++ src/MotoRent.Client/Services/MarkdownService.cs
@@ -1,17 +1,20 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using Markdig;
+using Microsoft.AspNetCore.Components;
 
 namespace MotoRent.Client.Services;
 
 public class MarkdownService
 {
     private readonly HttpClient m_httpClient;
+    private readonly NavigationManager m_navigationManager;
     private readonly MarkdownPipeline m_pipeline;
 
-    public MarkdownService(HttpClient httpClient)
+    public MarkdownService(HttpClient httpClient, NavigationManager navigationManager)
     {
         m_httpClient = httpClient;
+        m_navigationManager = navigationManager;
         m_pipeline = new MarkdownPipelineBuilder()
             .UseAdvancedExtensions()
             .Build();
@@ -21,7 +24,8 @@ public class MarkdownService
     {
         try
         {
-            var markdown = await m_httpClient.GetStringAsync(path);
+            var absoluteUri = m_navigationManager.ToAbsoluteUri(path).ToString();
+            var markdown = await m_httpClient.GetStringAsync(absoluteUri);
             return Markdown.ToHtml(markdown, m_pipeline);
         }
         catch (HttpRequestException)
