b33dd2b0a338441b7e4b3043827cd94f00ccb17a
Mang <mang@myhiro.my>
2026-03-16 12:36:38 +0700

    Fix Google Maps API loading by removing async loading parameter

--- src/MotoRent.Client/wwwroot/scripts/google-map.js
+++ src/MotoRent.Client/wwwroot/scripts/google-map.js
@@ -39,7 +39,7 @@ export function loadGoogleMapsApi(apiKey) {
         }
 
         const script = document.createElement('script');
-        script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&loading=async`;
+        script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`;
         script.async = true;
         script.defer = true;
         script.onload = () => {
